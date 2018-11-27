fa8d85372ca27c20858ad92e40d09a855f5eefcd
Kevin Miyata <kjm015@yahoo.com>
2018-11-27 10:36:10 -0600

    connect new view controller classes to storyboard

--- Quizzacious/QuizViewController.swift
+++ Quizzacious/QuizViewController.swift
@@ -1,23 +1,25 @@
 //
-//  ViewController.swift
+//  QuizViewController.swift
 //  Quizzacious
 //
-//  Created by Kevin Miyata on 11/20/18.
+//  Created by Kevin Miyata on 11/27/18.
 //  Copyright © 2018 Kevin Miyata. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var categoryPickerView: UIPickerView!
-    @IBOutlet weak var difficultyPickerView: UIPickerView!
+class QuizViewController: UIViewController {
+    
+    func configureView() {
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        configureView()
     }
-
-
+    
+    
 }
-
