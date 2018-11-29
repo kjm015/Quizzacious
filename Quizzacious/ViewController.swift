eab0c05a3399377283bc5d620b610639cf03cd72
Kevin Miyata <kjm015@yahoo.com>
2018-11-29 11:10:35 -0600

    fix api link issue

--- Quizzacious/ViewController.swift
+++ Quizzacious/ViewController.swift
@@ -123,10 +123,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBAction func quizStarted(_ sender: Any) {
         
         if (quizCategory == 0 && quizDifficulty.isEmpty) {
-        self.presentAlert(title: "Success", message: ("\(apiUrl)&difficulty\(quizDifficulty)&category\(quizCategory)"))
-        }
-        else {
             self.presentAlert(title: "Failed", message: ("Choose difficulty and category"))
+        } else {
+            self.presentAlert(title: "Success", message: ("\(apiUrl)&difficulty=\(quizDifficulty)&category=\(quizCategory)"))
         }
         
         //self.performSegue(withIdentifier: "Show Detail", sender: self)
